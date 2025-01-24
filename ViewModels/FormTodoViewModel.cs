eb1624f797c091e07d3f3e9db62d9b79b5e99fe7
fernando <luizfernandosr@outlook.com.br>
2025-01-23 23:04:03 -0300

    validação

--- ViewModels/FormTodoViewModel.cs
+++ ViewModels/FormTodoViewModel.cs
@@ -1,8 +1,11 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
 namespace TodoList.ViewModels;
     
 public class FormTodoViewModel
 {
+    [Required(ErrorMessage = "O título da tarefa é obrigatório.")]
     public string Title { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "A data é obrigatória.")]
     public DateTime Date { get; set; }
 }
