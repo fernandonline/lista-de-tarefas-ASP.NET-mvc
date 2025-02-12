d1f1295d29ca1d7edb933ef56dc85b37586db7dd
fernando <luizfernandosr@outlook.com.br>
2025-02-11 22:19:12 -0300

    date update

--- ViewModels/FormTodoViewModel.cs
+++ ViewModels/FormTodoViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
 namespace TodoList.ViewModels;
-
+    
 public class FormTodoViewModel
 {
+    public int Id { get; set; }
+
     [Required(ErrorMessage = "O título da tarefa é obrigatório.")]
     public string Title { get; set; } = string.Empty;
+
     [Required(ErrorMessage = "A data é obrigatória.")]
-    public DateTime Date { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime Date { get; set; } = DateTime.Today;
 }
